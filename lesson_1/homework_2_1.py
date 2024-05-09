9796c1a5748544c83716d31aee7b03d468760896
OligiEnlightener <oleh.viatkin@gmail.com>
2024-05-09 17:47:11 +0300

    "I have done everything simple"

--- lesson_1/homework_2_1.py
+++ lesson_1/homework_2_1.py
@@ -1,16 +1,12 @@
 def number_to_digits() -> list:
     print("Функция разбивает ввееденное вами число на цифры.")
     number = int(input("Введи 4х-значное число!\n"))
-    return [int(digit) for digit in list(str(number))]
+    print(int(number/1000))
+    number = number % 1000
+    print(int(number/100))
+    number = number % 100
+    print(int(number/10))
+    number = number % 10
+    print(number)
 
-
-
-
-
-def print_list(given):
-    for each in given:
-        print(each)
-
-
-list_to_print_1 = number_to_digits()
-print_list(list_to_print_1)
+number_to_digits()
--- lesson_1/homework_2_2.py
+++ lesson_1/homework_2_2.py
@@ -1,10 +1,19 @@
 def reverse_number() -> int:
+    # 12345
+    # 54321 = 50000 + 4000 + 300 + 20 + 1
     print("Функция возвращает перевернутое число.")
     number = int(input("Введи 5-значное число!\n"))
-    numbers = list(str(number))
-    numbers.reverse()
+    fifth = int(number % 10 * 10000)
+    number = number / 10
+    fourth = int(number % 10) * 1000
+    number = number / 10
+    third = int(number % 10) * 100
+    number = number / 10
+    second = int(number % 10) * 10
+    first = int(number / 10)
 
-    return int(''.join(str(digit) for digit in numbers))
+
+    return fifth + fourth + third + second + first
 
 
 result = reverse_number()
