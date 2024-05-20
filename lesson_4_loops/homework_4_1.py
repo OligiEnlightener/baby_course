c58fdd5071f40b4f436770a9f46c8dc00c78974c
OligiEnlightener <oleh.viatkin@gmail.com>
2024-05-20 17:39:27 +0300

    Я думаю ты хотел не этого, но думаю это даже лучше чем то, что ты имел ввиду

--- lesson_4_loops/homework_4_1.py
+++ lesson_4_loops/homework_4_1.py
@@ -1,13 +1,13 @@
 def move_zeros(lst: list):
-
-    for number in range(lst.count(0)):
-        lst.remove(0)
-        lst.append(0)
+    lst.sort(key=lambda x: x ==0)
     return lst
+
 
 test_list_1 = [0, 1, 3, 5, 0, 6, 7]
 test_list_2 = [1, 3, 5, 6, 3, 4]
 test_list_3 = []
+test_list_4 = [0, 1, 0, 4, 0, 1, 0, 6]
 print(move_zeros(test_list_1))
 print(move_zeros(test_list_2))
 print(move_zeros(test_list_3))
+print(move_zeros(test_list_4))
