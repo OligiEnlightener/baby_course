f7405077c5a3c11ca67141024fccb2774060d1a3
OligiEnlightener <oleh.viatkin@gmail.com>
2024-07-10 14:49:42 +0300

    9.3 : annotations added 10.2 : solution was simplified!

--- lesson_10_oop/homework_10_2.py
+++ lesson_10_oop/homework_10_2.py
@@ -1,18 +1,16 @@
-import math
+from inspect import isgenerator
 
 
 def generate_cube_numbers(end: int) -> list:
-    for n in range(2, end ):
+    for n in range(2, end):
         pow_n = n ** 3
         if pow_n <= end:
             yield pow_n
 
 
-from inspect import isgenerator
-
 gen = generate_cube_numbers(1)
-assert isgenerator(gen) == True, 'Test0'
+assert isgenerator(gen) is True, 'Test0'
 assert list(generate_cube_numbers(10)) == [8], f'оскільки воно менше 10. {list(generate_cube_numbers(10))}'
-assert list(generate_cube_numbers(100)) == [8, 27, 64], f'5 у кубі це 125, а воно вже більше 100. {list(generate_cube_numbers(100))}'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], (f'5 у кубі це 125, а воно вже більше 100. '
+                                                        f'{list(generate_cube_numbers(100))}')
 assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
-
